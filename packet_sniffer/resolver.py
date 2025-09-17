0e9e354c54d704aeca225db7126a3e807ef0ae44
Brandon Tupou <tupoubrandon@gmail.com>
2025-09-17 22:59:01 +0100

    Add cmd to format Python

--- packet_sniffer/resolver.py
+++ packet_sniffer/resolver.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 def main():
     setup_logging()
-    logging.info("Starting DNS resolver")
+    logger.info("Starting DNS resolver")
     while True:
         time.sleep(1)
         resolved = socket.gethostbyname("darktrace.com")
